37a10363f80b2cb421a739caffe72004cb1c7160
Cameron Fairchild <cameron@opentensor.ai>
2023-01-12 15:39:52 -0500

    use is_set for check_config

--- bittensor/_cli/commands/overview.py
+++ bittensor/_cli/commands/overview.py
@@ -333,7 +333,7 @@ class OverviewCommand:
 
     @staticmethod   
     def check_config( config: 'bittensor.Config' ):
-        if config.wallet.get('name') == bittensor.defaults.wallet.name  and not config.no_prompt and not config.all:
+        if not config.is_set('wallet.name') and not config.no_prompt and not config.all:
             wallet_name = Prompt.ask("Enter wallet name", default = bittensor.defaults.wallet.name)
             config.wallet.name = str(wallet_name)
 
