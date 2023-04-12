f5a0e5b5e8bd02abb2569904aa04003bdbce4000
Eugene <etesting007@gmail.com>
2023-04-12 16:33:34 -0700

    update removal

--- neurons/text/prompting/validators/core_rm/reward.py
+++ neurons/text/prompting/validators/core_rm/reward.py
@@ -30,10 +30,10 @@ class RewardModel(nn.Module):
     def __init__(self, model_path: str) -> None:
         super().__init__()
         #self.model = AutoModelForCausalLM.from_pretrained(model_path)
-        self.config = self.model.config
-        self.neox = "neox" in self.config.model_type
+        #self.config = self.model.config
+        #self.neox = "neox" in self.config.model_type
         # gpt-neo models have hidden_size instead of n_embd
-        self.config.n_embd = self.config.hidden_size if hasattr(self.config, "hidden_size") else self.config.n_embd
+        #self.config.n_embd = self.config.hidden_size if hasattr(self.config, "hidden_size") else self.config.n_embd
         #self.transformer = self.model.gpt_neox if hasattr(self.model, "gpt_neox") else self.model.transformer
         #dtype = self.config.torch_dtype if hasattr(self.config, "torch_dtype") is not None else torch.float32
         #dtype = torch.float16 if dtype == "float16" else torch.float32
